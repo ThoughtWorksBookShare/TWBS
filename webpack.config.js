28def4a250684443319d4997a244357595f8df63
code <1165444511@qq.com>
2017-03-20 22:51:22 +0800

    config babel and ignore bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,23 @@ let path = require("path");
 module.exports = {
     entry: "./public/entry.js",
     output: {
-        path: path.resolve(__dirname + "/public"),
+        path: path.resolve(__dirname + "/public/dist"),
         filename: "bundle.js"
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: "style!css" },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['react', 'es2015']
+                }
+            },
+            {test: /\.png$/, loader: "file-loader"},
+            {test: /\.jpg$/, loader: "file-loader"},
         ]
     }
 };
