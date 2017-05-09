cb9ceafcf328501f5f9c2d486d3cdf857d1ad0ad
code <1165444511@qq.com>
2017-05-09 13:40:29 +0800

    change the init state

--- public/js/reducers/signUp.js
+++ public/js/reducers/signUp.js
@@ -1,4 +1,4 @@
-export default (state = {result: false}, action)=> {
+export default (state = {result: 0}, action)=> {
     switch (action.type) {
         case "SIGNUP": {
             return {result: action.result}
