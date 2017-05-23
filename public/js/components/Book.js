6e1736bec3c218747118c35172293224595a3c93
yangzhanmei <15809244435@163.com>
2017-05-23 10:54:52 +0800

    modify the book's attributes' name

--- public/js/components/Book.js
+++ public/js/components/Book.js
@@ -1,35 +1,46 @@
 import React, {Component} from "react";
 
 export default class Book extends Component {
-    componentWillMount(){
+    componentWillMount() {
         const url = (window.location.href);
         const parms = url.split(":");
         const parmId = parms[parms.length - 1];
         this.props.getBookInfo(parmId);
     }
+
     render() {
+        const book = this.props.book;
+        let status = "";
+
+        if (!book.status) {
+            status = "未借出";
+        }
+        else {
+            status = "已借出";
+        }
+
         return (
             <div>
                 <div className="bookInfo">
                     <div className="bookPicture">
-                        <img src={this.props.book.bookPicture}/>
+                        <img src={book.bookPicture}/>
                     </div>
                     <div className="bookName">
-                        <h3>{this.props.book.bookName}</h3>
+                        <h3>{book.bookName}</h3>
                     </div>
                 </div>
                 <div className="bookInfo">
                     <div className="author">
-                        <span>作者：{this.props.book.author}</span>
+                        <span>作者：{book.bookAuthor}</span>
                     </div>
                     <div className="introduction">
-                        <span>简介：{this.props.book.introduction}</span>
+                        <span>简介：{book.bookIntroduction}</span>
                     </div>
                     <div className="bookOwner">
-                        <span>书主：{this.props.book.bookOwner}</span>
+                        <span>书主：{book.bookOwner}</span>
                     </div>
                     <div className="status">
-                        <span>状态：{this.props.book.status}</span>
+                        <span>状态：{status}</span>
                     </div>
                 </div>
                 <div className="comments">
