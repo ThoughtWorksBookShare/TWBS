4d8c9c58c90fbf69faef7f73ffda16c4e7dcdaba
yangzhanmei <15809244435@163.com>
2017-05-24 17:13:17 +0800

    show static information in comments component

--- public/js/components/ShowComments.js
+++ public/js/components/ShowComments.js
@@ -1,10 +1,20 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 
-export default class Commments extends Component{
-    render(){
+export default class Commments extends Component {
+    render() {
         return (
             <div>
-                comments
+                <div className="user">
+                    <div className="headUser" id="headPortrait"><img src="../../images/user.jpg"/></div>
+                    <div>用户名</div>
+                </div>
+                <div className="commentsInformation">
+                    <div>自控力</div>
+                    <div className="bookPicture"><img src="../../images/logo.jpg"/></div>
+                    <div><p>白颖嘿嘿嘿</p></div>
+                    <div>时间</div>
+                    <div>点赞哦</div>
+                </div>
             </div>
         )
     }
