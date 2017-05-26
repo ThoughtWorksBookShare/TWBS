8000974719e93f08165d69b061ddadba451a770b
liuxing-github <1725366795@qq.com>
2017-05-26 18:37:28 -0400

    add buttun for wantedBooks

--- public/js/components/WantedBooks.js
+++ public/js/components/WantedBooks.js
@@ -1,10 +1,14 @@
 import React,{Component} from 'react';
 
 export default class WantedBooks extends Component{
+
+    turn(){
+        window.location = "#/wantedBooks/addDesiredBook";
+    }
     render(){
         return(
             <div>
-                WantedBooks
+               <button className="uploadDesiredBook" onClick={this.turn}>我还想看</button>
             </div>
         )
     }
