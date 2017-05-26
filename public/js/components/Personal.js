b3983e845cf4f7cce6a87f47fb212122252c096a
yangzhanmei <15809244435@163.com>
2017-05-26 20:46:00 +0800

    add sidebar to Personal

--- public/js/components/Personal.js
+++ public/js/components/Personal.js
@@ -1,11 +1,23 @@
 import React,{Component} from 'react';
-import Header from "./Header";
 
 export default class Personal extends Component{
     render(){
         return(
             <div>
-                Personal
+                <Sidebar/>
+            </div>
+        )
+    }
+}
+
+class Sidebar extends Component{
+    render(){
+        return (
+            <div id="sideBar">
+                <div><button>个人信息</button></div>
+                <div><button>我的书库</button></div>
+                <div><button>我想看的书</button></div>
+                <div><button>我的评论</button></div>
             </div>
         )
     }
