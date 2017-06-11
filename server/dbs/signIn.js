2d2faa03e88afde5f3b040529d249d61e77bb4df
yangzhanmei <15809244435@163.com>
2017-06-11 22:15:37 +0800

    modifay users collection

--- server/dbs/signIn.js
+++ server/dbs/signIn.js
@@ -3,9 +3,9 @@ const cn = require('./connect');
 function verifyInfo(req, callback) {
     cn.MongoClient.connect(cn.url, (err, db)=> {
         const collection = db.collection('users');
-        collection.find({name: req.body.inputName}).toArray((err, result)=> {
+        collection.find({"personalMessage.name": req.body.inputName}).toArray((err, result)=> {
             let flag = false;
-            if (result.length > 0 && result[0].password == req.body.inputPass) {
+            if (result.length > 0 && result[0].personalMessage.password == req.body.inputPass) {
                 flag = true;
             }
             callback(flag);
